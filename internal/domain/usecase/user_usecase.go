ecb20bf8d80175557585c14d2d8962218ebc1371
Alisher Arginbekov <alisher.arginbekov@mail.ru>
2026-03-29 18:36:22 +0500

    ListAll fixed

--- internal/domain/usecase/user_usecase.go
+++ internal/domain/usecase/user_usecase.go
@@ -12,7 +12,6 @@ type UserUseCase interface {
 	GetByID(ctx context.Context, id uuid.UUID) (*entity.User, error)
 	Update(ctx context.Context, user *entity.User) (*entity.User, error)
 	Delete(ctx context.Context, id uuid.UUID) error
-	ListAll(ctx context.Context, limit, offset int) ([]*entity.User, int, error)
 	AdminUpdateUser(ctx context.Context, id uuid.UUID, role *entity.Role, libraryID *uuid.UUID, name, surname, email, phone *string) (*entity.UserView, error)
 	AdminDelete(ctx context.Context, id uuid.UUID) error
 	UploadAvatar(ctx context.Context, id uuid.UUID, avatar *fileupload.File) (*entity.User, error)
